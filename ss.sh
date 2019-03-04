8ef9992ee5954a4b2260ba2b4045d4cb756b5fd7
Tianyi Wang <tywang666@gmail.com>
2019-03-04 18:25:32 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 wget --no-check-certificate -O shadowsocks.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks.sh
-chmod +x shadowsocks-libev.sh
-./shadowsocks-libev.sh 2>&1 | tee shadowsocks-libev.log
+chmod +x shadowsocks.sh
+./shadowsocks.sh 2>&1 | tee shadowsocks.log
