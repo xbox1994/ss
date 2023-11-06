63132a1fb2ccadf5a5ed606a6852f701a102015b
DMC <tywang666@gmail.com>
2023-11-06 19:34:33 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -22,3 +22,5 @@ wget https://github.com/shadowsocks/v2ray-plugin/releases/download/v1.3.2/v2ray-
 tar zxf v2ray-plugin-linux-amd64-v1.3.2.tar.gz
 mv v2ray-plugin_linux_amd64 /usr/bin/v2ray-plugin
 service shadowsocks-libev restart
+
+ufw disable
